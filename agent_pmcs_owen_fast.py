043c10e2c871fa63ae26a5ecd66d5153ac703cd3
Owen-Deng <starnew@126.com>
2024-04-26 08:44:12 -0500

    update

---
+++ agent_pmcs_owen_fast.py
@@ -0,0 +1,110 @@
+import math
+import numpy as np
+from scipy.signal import convolve2d
+from scipy.special import softmax
+import random
+
+ROW, COL = 6, 7
+
+class PMCSAgent:
+    def __init__(self, inc = 1000):
+        horizontal_kernel = np.array([[1, 1, 1, 1]])
+        vertical_kernel = np.transpose(horizontal_kernel)
+        diag1_kernel = np.eye(4, dtype=np.uint8)
+        diag2_kernel = np.fliplr(diag1_kernel)
+        self.detection_kernels = [horizontal_kernel,
+                                vertical_kernel, diag1_kernel, diag2_kernel]
+        self.inc = inc
+            
+    def playout(self, state, action, player = 1):
+        """Perfrom a random playout starting with the given action on the given board 
+        and return the utility of the finished game."""
+        state = state.copy()
+        action_dict = self.action_dict.copy()
+        action_keys = self.action_keys.copy()
+
+        num_discs = self.num_discs
+        #action_history = []
+        current_player = player
+        while(True):
+            state[action_dict[action]-1, action] = current_player
+            num_discs += 1
+            #action_history.append(action)
+            # reached terminal state?
+            if num_discs >= 7:
+                #tuple_action_history = state.tobytes()
+                # if tuple_action_history in self.cache:
+                #     u = self.cache[tuple_action_history]
+                # else:
+                u = 0
+                for kernel in self.detection_kernels:
+                    a = convolve2d(state, kernel, mode='valid')
+                    if ((a == 4).any()):
+                        u = 1 if player == 1 else -1
+                        break
+                    if ((a == -4).any()):
+                        u = 1 if player == -1 else -1
+                        break
+                    #self.cache[tuple_action_history] = u
+    
+                if u != 0 or num_discs == 42:
+                    return u
+
+            action_dict[action] -= 1
+            if action_dict[action] == 0:
+                action_keys.remove(action)
+                
+            action = random.choice(action_keys)
+        
+            # switch between players
+            current_player = -current_player
+    
+    def playouts(self, board, action, player, N):
+        """Perform N playouts following the given action for the given board."""
+        return [self.playout(board, action, player) for _ in range(N)]
+
+
+    def act(self, board, player = 1):
+        """Pure Monte Carlo Search. Returns the action that has the largest average utility.
+        The N playouts are evenly divided between the possible actions."""
+        
+        self.num_discs = len(np.where(board != 0)[0])
+        if self.num_discs == 0:
+            return 3
+        
+        N = (self.num_discs+1) * self.inc
+        
+        self.action_dict = {}
+        for col in range(COL):
+            num_zeros = len(np.where(board[:, col] == 0)[0])
+            if num_zeros == 0:
+                continue
+            self.action_dict[col] = num_zeros
+        self.action_keys = list(self.action_dict)
+        
+        n = math.floor(N/COL)
+        #self.cache = {}
+        ps = [[i, np.mean(self.playouts(board, i, player, N = n))] for i in self.action_dict]
+        
+        if len(ps) == 1:
+            return ps[0][0]
+        ps = sorted(ps, key=lambda x: x[1], reverse=True)
+        if self.num_discs < 21 and ps[1][1] > 0:
+            for col, _ in ps[:2]:
+                row_idx = self.action_dict[col]-1
+                if row_idx % 2 == 0 and player == -1:
+                    return col
+                elif row_idx % 2 == 1 and player == 1:
+                    return col
+        
+        return ps[0][0]
+
+if __name__ == "__main__":
+    import time
+    a = PMCSAgent(15000)
+    board = np.zeros((ROW, COL), dtype=np.int8)
+    st = time.time()
+    a.act(board, 1)
+    ut = time.time() - st
+    print(ut)
+    # 1 odd -1 even
